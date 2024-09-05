95b2b6c850deccb294ebd6bc7fdb76d7795704bf
wesleyDM14 <wesleyfga@hotmail.com>
2024-09-05 16:07:55 -0300

    Create a router structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router } from "react-router-dom";
+import { connect } from "react-redux";
 
-function App() {
+import { AppRoutes } from "./routes";
+
+const App = ({ checked }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {checked && <AppRoutes />}
+    </Router>
   );
 }
 
-export default App;
+const mapStateToProps = ({ session }) => ({
+  checked: session.checked
+});
+
+export default connect(mapStateToProps)(App);
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+export const AppRoutes = () => (
+    <Routes>
+
+    </Routes>
+);
